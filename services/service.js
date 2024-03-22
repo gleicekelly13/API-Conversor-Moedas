5e522549ae60b091c524b591f9d2e2e09329f810
gleicekelly13 <gleice.kelly22@hotmail.com>
2024-03-22 11:28:17 -0300

    Criação da função que converte euro para real

--- services/service.js
+++ services/service.js
@@ -5,19 +5,27 @@ function conversaoMoeda(moeda, valor) {
         resultado = converteParaDolar(valor);
     } else if (moeda === 'R$') {
         resultado = converteParaReal(valor); 
+    } else if (moeda === '€') {
+        resultado = converteEuroParaReal(valor);
     }
+
 
     return resultado;
 }
 
-function converteParaReal(USD) {
+function converteParaReal (USD) {
     let BRL = USD * 5.00;
     return BRL;
 };
 
-function converteParaDolar(BRL) {
+function converteParaDolar (BRL) {
     let USD = BRL * 0.20;
     return USD;
+}
+
+function converteEuroParaReal (EUR) {
+    let BRL = EUR * 5.40;
+    return BRL;
 }
 
 exports.conversaoMoeda = conversaoMoeda;
